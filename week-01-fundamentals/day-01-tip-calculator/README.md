37adefdebdcabd0ab0460cf5f9fe548fbbe579dd
sahildevgun702 <devgun.sahil@gmail.com>
2025-06-24 00:02:34 -0400

    initial commit 2

---
+++ week-01-fundamentals/day-01-tip-calculator/README.md
@@ -0,0 +1,3 @@
+Create a program that calculates the tip for a restaurant bill
+Input: Bill amount
+Output: Total amount to pay
---
+++ week-01-fundamentals/day-01-tip-calculator/solution.js
@@ -0,0 +1,5 @@
+function tipCalculator (amount, tip) {
+    return amount + (amount * tip/100);
+}
+
+console.log(tipCalculator(10,5));
